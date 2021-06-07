0f52beda8fa0f95511407634a378b5b9992c68c2
jasko255 <jasko255@gmail.com>
2021-06-07 16:36:12 +0200

    empty comments

---
+++ books/src/components/CommentArea.jsx
@@ -0,0 +1,38 @@
+import {Component} from 'react'
+// import CommentList from './CommentList'
+
+
+class CommentArea extends Component {
+
+        state = {
+            comments:[]
+        }
+
+        componentDidMount = async () => {
+            try {
+                let response = await fetch ('https://striveschool-api.herokuapp.com/api/comments/' + this.props.asin , {
+                    headers: {
+                        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGFlNWM1ZGNlYWY0ODAwMTVjOTE5NDEiLCJpYXQiOjE2MjMwNzQzNzEsImV4cCI6MTYyNDI4Mzk3MX0.rVnzNrsnvKbrU4pU19NYKXIInjvv5PBZkWEAOTqkYTY'
+                    }
+                })
+                console.log(response);
+                let comments = await response.json()
+                console.log(comments);
+                this.setState({comments: comments})
+            } catch (error) {
+                console.log(error);
+                
+            }
+        }
+
+    render() {
+        return (
+            <div>
+                {/* <CommentList commentsToShow={this.state.comments} /> */}
+                COMMENTAERA
+            </div>
+        );
+    }
+}
+
+export default CommentArea;
